bdeacfe7237b92ce4c8a65e7adbdac500e923364
‘zhan <975857725@qq.com>
2022-08-24 03:51:00 +0800

    init

---
+++ dist/main.59103ee5.js
@@ -0,0 +1,305 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"main.js":[function(require,module,exports) {
+var html = document.querySelector('#html');
+var style = document.querySelector("#style");
+
+var string = "\n/* hi\uFF0C\u81ED\u72D7\u2665\n * \u63A5\u4E0B\u6765\u6211\u8981\u9001\u4F60\u4E00\u4E2A\u5C0F\u5FC3\u5FC3\n * \u9996\u5148\u6211\u8981\u51C6\u5907\u4E00\u4E2A\u770B\u4E0D\u89C1\u7684\u5927\u76D2\u5B50\u6765\u88C5\u201C\u5C0F\u5FC3\u5FC3\u201D\n **/\n#heart {\n    width: 200px;\n    height: 170px;\n    \n}\n/* \u7136\u540E\u6211\u51C6\u5907\u4E00\u4E2A\u5FC3\u5FC3\u7684\u5DE6\u4E0A\u89D2 */\n#left {\n    background:red;\n    width: 100px;\n    height: 100px;\n    border-radius: 50%;\n    transform: rotate(45deg) translateX(21px)\n}\n/* \u63A5\u4E0B\u6765 \u51C6\u5907\u5FC3\u5FC3\u7684\u53F3\u4E0A\u89D2 */\n#right {\n    width: 100px;\n    height: 100px;\n    background:red;\n    border-radius: 50%;\n    transform: rotate(45deg) translateY(21px)\n}\n/* \u7136\u540E\u5728\u4E0B\u9762\u753B\u4E00\u4E2A\u6B63\u65B9\u5F62*/\n#bottom {\n    width: 100px;\n    height: 100px;\n    background:red;\n    transform: rotate(45deg);\n}\n/* \u7136\u540E\u6211\u8BA9\u8FD9\u4E2A\u201C\u5FC3\u52A8\u201D\u8D77\u6765 */\n#heart {\n    animation:heart 800ms infinite alternate;\n}\n@keyframes heart {\n    0% {\n        transform: scale(1.0);\n    }\n    100% {\n        transform: scale(1.3);\n    }\n}\n/*\n *okk\n *\u73B0\u5728\uFF0C\u4E00\u4E2A\u5C5E\u4E8E\u4F60\u7684\u5C0F\u5FC3\u5FC3\u5C31\u505A\u597D\u5566\n *\u2665\u7231\u4F60\u54E6\uFF01\u2665\n**/\n";
+var string2 = "";
+var n = 0;
+
+var step = function step() {
+    setTimeout(function () {
+        if (string[n] === "\n") {
+            string2 += "<br>";
+        } else if (string[n] === " ") {
+            string2 += "&nbsp;";
+        } else {
+            string2 += string[n];
+        }
+        html.innerHTML = string2;
+        style.innerHTML = string.substring(0, n);
+        window.scrollTo(0, 99999);
+        html.scrollTo(0, 999);
+        if (n < string.length - 1) {
+            n += 1;
+            step();
+        }
+    }, 50);
+};
+step();
+},{}],"..\\..\\..\\..\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = '' || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '52635' + '/');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      console.clear();
+
+      data.assets.forEach(function (asset) {
+        hmrApply(global.parcelRequire, asset);
+      });
+
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.parcelRequire, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+
+      removeErrorOverlay();
+
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAccept(global.parcelRequire, id);
+  });
+}
+},{}]},{},["..\\..\\..\\..\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js","main.js"], null)
+//# sourceMappingURL=/main.59103ee5.map
---
+++ src/main.js
@@ -0,0 +1,77 @@
+let html = document.querySelector('#html');
+let style = document.querySelector("#style");
+
+let string = `
+/* hi，臭狗♥
+ * 接下来我要送你一个小心心
+ * 首先我要准备一个看不见的大盒子来装“小心心”
+ **/
+#heart {
+    width: 200px;
+    height: 170px;
+    
+}
+/* 然后我准备一个心心的左上角 */
+#left {
+    background:red;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    transform: rotate(45deg) translateX(21px)
+}
+/* 接下来 准备心心的右上角 */
+#right {
+    width: 100px;
+    height: 100px;
+    background:red;
+    border-radius: 50%;
+    transform: rotate(45deg) translateY(21px)
+}
+/* 然后在下面画一个正方形*/
+#bottom {
+    width: 100px;
+    height: 100px;
+    background:red;
+    transform: rotate(45deg);
+}
+/* 然后我让这个“心动”起来 */
+#heart {
+    animation:heart 800ms infinite alternate;
+}
+@keyframes heart {
+    0% {
+        transform: scale(1.0);
+    }
+    100% {
+        transform: scale(1.3);
+    }
+}
+/*
+ *okk
+ *现在，一个属于你的小心心就做好啦
+ *♥爱你哦！♥
+**/
+`;
+let string2 = "";
+let n = 0;
+
+let step = () => {
+    setTimeout(() => {
+        if (string[n] === "\n") {
+            string2 += "<br>";
+        } else if (string[n] === " ") {
+            string2 += "&nbsp;";
+        } else {
+            string2 += string[n];
+        }
+        html.innerHTML = string2;
+        style.innerHTML = string.substring(0, n);
+        window.scrollTo(0, 99999);
+        html.scrollTo(0, 999);
+        if (n < string.length - 1) {
+            n += 1;
+            step();
+        }
+    }, 50);
+};
+step();
